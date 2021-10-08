7010eac4d2b36ca3f1c8824cd9f3ea2582cadd03
doudeau <tho.doudeau@gmail.com>
2021-10-08 16:23:41 +0200

    test3

--- python/receiver.py
+++ python/receiver.py
@@ -1,36 +1,59 @@
-
-
 import boto3
 
 # Create SQS client
 sqs = boto3.client('sqs')
-
-queue_url = 'https://queue.amazonaws.com/976055184967/test'
-while True :
-    # Receive message from SQS queue
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
-        AttributeNames=[
-            'SentTimestamp'
-        ],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=[
-            'All'
-        ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
-    )
+def create_queue():
     try :
-        message = response['Messages'][0]
-        receipt_handle = response['ReceiptHandle']
-        numbers = message['MessageAttributes']['Numbers']['StringValue']
-
-        sqs.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle
-    )
+        queue = sqs.get_queue_by_name(QueueName='responseQueue')
     except :
-        continue
+        queue = sqs.create_queue(QueueName='responseQueue', Attributes={'DelaySeconds': '5'})
+    return queue
 
-    print(numbers)
+def receive(queue) :
+    while True :
+        # Receive message from SQS queue
+        response = sqs.receive_message(
+            QueueUrl=queue,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
+        try :
+            message = response['Messages'][0]
+            receipt_handle = response['ReceiptHandle']
+            numbers = message['MessageAttributes']['Numbers']['StringValue']
 
+            sqs.delete_message(
+            QueueUrl=queue,
+            ReceiptHandle=receipt_handle
+            )
+
+            print(numbers)
+            return numbers
+        except :
+            print("attente")
+
+
+
+def send(queue):
+    response = queue.send_message(
+        MessageBody='boto3', 
+        MessageAttributes={
+            'Numbers': {
+                'StringValue': 'oui',
+                'DataType': 'String'
+                    }
+    })
+
+def main():
+    queue = create_queue()
+    receive('https://sqs.us-east-1.amazonaws.com/976055184967/requestQueue')
+    send(queue)
+
+main()
--- python/test.py
+++ python/test.py
@@ -5,7 +5,11 @@ sqs = boto3.resource('sqs')
 
 def create_queue():
     # Create the queue. This returns an SQS.Queue instance
-    return sqs.create_queue(QueueName='test', Attributes={'DelaySeconds': '5'})
+    try :
+        queue = sqs.get_queue_by_name(QueueName='requestQueue')
+    except :
+        queue = sqs.create_queue(QueueName='requestQueue', Attributes={'DelaySeconds': '5'})
+    return queue
     
 
 def send_numbers(numbers,queue) :
@@ -19,12 +23,51 @@ def send_numbers(numbers,queue) :
                     }
     })
     # You can now access identifiers and attributes
-    print(queue.url)
+    #print(queue.url)
 
+def receive():
+    while True :
+        try :
+            queue = sqs.get_queue_by_name(QueueName='requestQueue')
+        except:
+            print("attente")
+
+        try :
+            response = sqs.receive_message(
+                QueueUrl=queue,
+                AttributeNames=[
+                    'SentTimestamp'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ],
+                VisibilityTimeout=0,
+                WaitTimeSeconds=0
+            )
+        except :
+            print('ici')
+        try :
+            message = response['Messages'][0]
+            receipt_handle = response['ReceiptHandle']
+            res = message['MessageAttributes']['Numbers']['StringValue']
+
+            sqs.delete_message(
+            QueueUrl=queue,
+            ReceiptHandle=receipt_handle
+            )
+            return res
+
+        except Exception as e :
+            print(e)
+
+    
 
 def main():
     queue = create_queue()
     send_numbers('2,5,6,3,8,9,4,6,5,5,8', queue)
+    res = receive()
+    print(res)
 
 main()
 
